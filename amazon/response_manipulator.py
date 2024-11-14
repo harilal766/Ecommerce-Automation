9a9e0a5dc9488a13ef0b8bbdba4ec8a608be1449
harilal766 <harilalsunil2@gmail.com>
2024-11-14 20:09:59 +0530

    amazon summary : set -> list

--- amazon/response_manipulator.py
+++ amazon/response_manipulator.py
@@ -6,22 +6,26 @@ def next_shipment_summary(response):
         out_list = response['payload']['Orders']
         today_string = str(datetime.today()).split(" ")[0]
         cod_orders = []; prepaid_orders = []
-        order_count = 0; cod_count = 0; prepaid_count = 0
+        order_count = 0; cod_count = 0; prepaid_count = 0; 
         for item in out_list:
-            ship_date_string = str(item['EarliestShipDate']).split("T")[0]
-            #ship_date_string = today_string
+            #ship_date_string = str(item['EarliestShipDate']).split("T")[0]
+            ship_date_string = today_string
             last_update_date_string = str(item[ "LastUpdateDate"]).split("T")[0]
             print(f"Ship date : {ship_date_string},  Today : {today_string} :- {ship_date_string == today_string}")
             if (type(item) == dict):
                 order_count += 1
                 color_print(message=f"Order : {order_count}{'-'*40}",color='blue')
+                field_count = 0
                 for key,value in item.items():
+                    
                     if  today_string == ship_date_string: 
+                        field_count+=1
+                        order_id = f"{field_count} - {item['AmazonOrderId']}"
                         if item['PaymentMethodDetails'] == ['CashOnDelivery']:
-                            cod_orders.append(item['AmazonOrderId'])
+                            cod_orders.append(order_id)
                         elif item['PaymentMethodDetails'] == ['Standard']:
-                            prepaid_orders.append(item['AmazonOrderId'])
-                        print(f"{key} : {value}")
+                            prepaid_orders.append(order_id)
+                        print(f"{field_count}.{key} : {value}")
                     
         color_print(message=f"COD : {cod_orders} \n Prepaid : {prepaid_orders}",color='blue')
     except Exception as e:
