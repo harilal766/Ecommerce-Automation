4811f12ca55517eae40277320750246d3c7cbed9
harilal766 <harilasunil2@gmail.com>
2024-11-25 17:05:49 +0530

    issues resolved for django

--- sales/views.py
+++ sales/views.py
@@ -8,5 +8,5 @@ def home(request):
     ord_ins = Orders(); created_after = (datetime.utcnow() - timedelta(days=7)).isoformat()
     ord_resp = ord_ins.getOrders(CreatedAfter=created_after,OrderStatuses="Unshipped")
 
-    context = {'order_count' : ord_resp}
+    context = {'orders' : ord_resp}
     return render(request,'home.html',context)
--- spapi.py
+++ spapi.py
@@ -1,8 +1,2 @@
 from amazon.authorization import *
-
-
-"""
-ord_ins = Orders(); created_after = (datetime.utcnow() - timedelta(days=7)).isoformat()
-ord_resp = ord_ins.getOrders(CreatedAfter=created_after,OrderStatuses="Unshipped")
-print(ord_resp)
-"""
+from amazon.api_models import *
